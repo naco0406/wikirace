97692bea8eeaeea4e3b9ec542d528373067909b5
naco0406 <young0406@kaist.ac.kr>
2024-08-05 22:45:37 +0900

    refactor: StartScreen

--- src/components/Start/Screen.tsx
+++ src/components/Start/Screen.tsx
@@ -26,6 +26,7 @@ import { getKSTDateString } from '@/lib/firebaseConfig';
 import { Trophy, Move, Clock } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useKeyboard } from '@/hooks/useKeyboard';
+import Logo from '@/assets/Logo';
 
 const StartScreen: React.FC = () => {
     const [dailyChallenge, setDailyChallenge] = useState<DailyChallenge | null>(null);
@@ -62,99 +63,22 @@ const StartScreen: React.FC = () => {
     }, []);
 
     return (
-        <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white p-4">
-            <p className="text-lg mb-4 text-center text-white">안녕하세요, {nickname}님!</p>
-            <Card className="w-full max-w-lg bg-white text-gray-800 shadow-lg rounded-lg overflow-hidden">
-                <CardHeader className="p-6">
-                </CardHeader>
-                <CardContent className="p-6">
-                    <h2 className="text-xl mb-4"><strong>오늘의 챌린지</strong> {today}</h2>
-                    <p className="text-sm mt-2">{`오늘 완료한 사람 수: ${dailyChallenge ? dailyChallenge.totalCount : '-'}`}</p>
-
-                    <div className="space-y-4 mt-6">
-                        {!(hasClearedToday || hasGiveUpToday) ? (
-                            <Link href="/game" className="block">
-                                <Button className="w-full text-lg p-6">
-                                    {hasStartedToday ? '게임 이어서 도전하기' : '게임 시작'}
-                                </Button>
-                            </Link>
-                        ) : (
-                            <Button className="w-full text-lg p-6 bg-gray-400 cursor-not-allowed" disabled>
-                                {hasClearedToday ? '오늘의 도전을 완료했습니다!' : '오늘의 도전을 포기했습니다.'}
-                            </Button>
-                        )}
-                        {(hasClearedToday || hasGiveUpToday) && (
-                            <Fragment>
-                                {!hasGiveUpToday && (
-                                    <div className="w-full border border-black p-6 text-black border rounded-lg bg-gradient-to-r from-blue-50 to-purple-50">
-                                        <div className='flex flex-col justify-between items-center'>
-                                            <p><strong>시작:</strong> {dailyChallenge ? dailyChallenge.startPage : '-'}</p>
-                                            <p className="my-2">↓</p>
-                                            <p><strong>목표:</strong> {dailyChallenge ? dailyChallenge.endPage : '-'}</p>
-                                            {localRecord && (
-                                                <div className="w-full mt-4 pt-4 border-t border-gray-300">
-                                                    <div className="flex items-center justify-center mb-2">
-                                                        <Trophy className="w-5 h-5 text-yellow-500 mr-2" />
-                                                        <span className="font-bold text-lg">오늘 나의 기록</span>
-                                                    </div>
-                                                    <div className="flex justify-center space-x-4">
-                                                        <div className="flex items-center">
-                                                            <Move className="w-4 h-4 text-blue-500 mr-1" />
-                                                            <span>{localRecord.moveCount}번 이동</span>
-                                                        </div>
-                                                        <div className="flex items-center">
-                                                            <Clock className="w-4 h-4 text-green-500 mr-1" />
-                                                            <span>{formatTime(localRecord.time)}</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            )}
-                                        </div>
-                                    </div>
-                                )}
-                                {/* <Link href="/ranking" className="block">
-                                    <p className="text-sm mt-2 text-center text-gray">
-                                        랭킹 보기
-                                    </p>
-                                </Link> */}
-                            </Fragment>
-                        )}
-                        <Dialog open={open} onOpenChange={setOpen}>
-                            <DialogTrigger asChild>
-                                <Button variant="outline" className="w-full text-lg p-6">
-                                    링클이 무엇인가요?
-                                </Button>
-                            </DialogTrigger>
-                            <DialogContent>
-                                <DialogHeader>
-                                    <DialogTitle>링클, 매일 새로운 위키피디아 스피드런</DialogTitle>
-                                    <DialogDescription>
-                                        Linkle, a new Wikipedia speedrun every day
-                                    </DialogDescription>
-                                </DialogHeader>
-                                <Carousel className="w-full max-w-xs mx-auto">
-                                    <CarouselContent>
-                                        {qaPairs.map((qa, index) => (
-                                            <CarouselItem key={index}>
-                                                <div className="p-4">
-                                                    <h3 className="font-bold mb-2">{qa.question}</h3>
-                                                    <p>{qa.answer}</p>
-                                                </div>
-                                            </CarouselItem>
-                                        ))}
-                                    </CarouselContent>
-                                    <CarouselPrevious />
-                                    <CarouselNext />
-                                </Carousel>
-                            </DialogContent>
-                        </Dialog>
-                    </div>
-                </CardContent>
-            </Card>
-            <Button variant="outline" className="w-full max-w-lg text-lg mt-4 px-6 bg-transparent" onClick={() => router.push('/yesterday')}>
-                어제의 기록
-            </Button>
-            <p className="text-xs mt-8 text-center text-white">© 2024 Naco & Minseo Lim. All rights reserved.</p>
+        <div className="min-h-screen w-full flex flex-col items-center justify-center bg-white overflow-hidden">
+            <p className="mt-[28px] mb-[100px]" />
+            <Logo width={250} height={75} />
+            <p className="font-[400] text-[24px] leading-[28px] mt-[28px] mb-[100px]">{`오늘 완료한 사람 수 : ${dailyChallenge ? dailyChallenge.totalCount : '-'}`}</p>
+            {!(hasClearedToday || hasGiveUpToday) ? (
+                <Link href="/game" className="block">
+                    <Button className="w-full text-lg bg-linkle p-6">
+                        {hasStartedToday ? '이어서 도전하기' : '시작'}
+                    </Button>
+                </Link>
+            ) : (
+                <Button className="w-full text-lg p-6 bg-linkle cursor-not-allowed" disabled>
+                    {hasClearedToday ? '오늘의 도전을 완료했습니다!' : '오늘의 도전을 포기했습니다.'}
+                </Button>
+            )}
+            <p className="text-xs mt-[300px] text-center text-[#495057]">© 2024 Naco & Minseo Lim. All rights reserved.</p>
         </div>
     );
 };
