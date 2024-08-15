a2f1eb6c25df3fec3b3ad3ebc21050158e2add19
naco0406 <young0406@kaist.ac.kr>
2024-08-15 21:14:53 +0900

    feat: Vercel Analytics 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import { TimerProvider } from '@/contexts/TimerContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "링클 - Linkle",
@@ -22,6 +23,7 @@ export default function RootLayout({
         <TimerProvider>
           {children}
         </TimerProvider>
+        <Analytics />
       </body>
     </html>
   );
